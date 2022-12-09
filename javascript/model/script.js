e161b7ce192186d299704ac9dbc88170f85c761f
fuadul hasan <53356319+fuad7161@users.noreply.github.com>
2022-12-09 08:52:11 +0600

    Add files via upload

---
+++ javascript/model/script.js
@@ -0,0 +1,21 @@
+'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCoseModel = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModal);
+}
+
+btnCoseModel.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
