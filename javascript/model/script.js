226f628107c8c798c05e507e55270bbdf782bb8c
fuadul hasan <53356319+fuad7161@users.noreply.github.com>
2022-12-09 09:21:07 +0600

    updated

--- javascript/model/script.js
+++ javascript/model/script.js
@@ -19,3 +19,8 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
 
 btnCoseModel.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
